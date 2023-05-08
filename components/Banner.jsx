e7ea517b3387e7b038538b3aa91c21fa077604bf
QaiserHussain <qaiserh844@gmail.com>
2023-05-08 23:12:37 +0500

    issues resolved

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { Bruno_Ace_SC, Inter } from 'next/font/google';
+import { Bruno_Ace_SC} from 'next/font/google';
 import Image from 'next/image';
 
 const bruno = Bruno_Ace_SC({ weight: ['400'], subsets: ['latin'] });
-const inter = Inter({ subsets: ['latin'] });
+
 export default function Banner() {
     return (
         <div id='home' className='w-full xs:h-4/6 sm:h-4/6 md:h-4/6 flex'>
             <div className='flex-1 h-full flex items-end'>
                 <div>
                     <h1 className={`text-5xl ${bruno.className}`}>Qaiser Hussain</h1>
-                    <p className={`text-2xl mt-3 ${bruno.className}`}>I'm a Full Stack <span className='tracking-widest'> MERN </span> Developer</p>
+                    <p className={`text-2xl mt-3 ${bruno.className}`}>I am a Full Stack <span className='tracking-widest'> MERN </span> Developer</p>
                     {/* <p className='text-gray-500 text-justify pr-20 mt-3'>A dedicated and enthusiastic MERN stack developer with experience building responsive web applications.
                         Proficient in creating web applications using MongoDB, Express.js,
                         React.js, Next.js, and Node.js. Passionate about developing user-friendly
